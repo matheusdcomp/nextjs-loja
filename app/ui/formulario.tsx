f7ef4111551e7affcf4f47a7a56156cf5632f604
Matheus Viana <matheuscviana@ufsj.edu.br>
2024-07-08 20:43:33 -0300

    Correcao nos estados do formulario

--- app/ui/formulario.tsx
+++ app/ui/formulario.tsx
@@ -72,15 +72,17 @@ export default function Formulario({
     inputs.forEach(i => valores.push(i.value));
 
     if (estado == "adicionando") funAdicionar(valores);
-    else if (estado == "adicionando") funEditar(valores);
+    else if (estado == "editando") funEditar(valores);
 
     limparFormulario();
     mostrarFormulario("none");
+    setEstado("parado");
   }
 
   function cliqueCancelar() {
     limparFormulario();
     mostrarFormulario("none");
+    setEstado("parado");
   }
 
   const inputs = entidadeUIProps.map(ipt => (
